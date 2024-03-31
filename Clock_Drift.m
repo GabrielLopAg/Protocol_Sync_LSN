a01ec65b7dfdb673cda36201d19d7dbd8e222e3e
Gabriel <gla280900@gmail.com>
2024-03-31 16:34:52 -0600

    fix clock drift

--- Clock_Drift.m
+++ Clock_Drift.m
@@ -1,29 +1,24 @@
-num_nodes = 10; % Número de nodos
-sim_duration = 3600; 
-time_step = 0.01; % Duración en el que el reloj es estable
-mean_sender_delay = 100e-6; % Mean Tx delay (seg)
-std_sender_delay = 11e-6; % Standard deviation Tx delay (seg)
-mean_receiver_delay = 100e-6; % Mean receiver delay (seg)
-std_receiver_delay = 11e-6; % Standard deviation of receiver delay (seg)
-crystal_freq_mean = 7.3738e6; % Mean crystal oscillator frequency (Hz)
-crystal_freq_std = 100e3; % Standard deviation of crystal oscillator frequency (Hz)
+num_nodes = 10;
+sampling_rate = 1; % Hz
+duration = 3600; % segundos
+drift_rate = 40e-6; % μs per second
+crystal_freq_mean = 7.37e6; % Hz
 
-time = 0:time_step:sim_duration;
-clock_drift = zeros(num_nodes, length(time));
-sender_delay = normrnd(mean_sender_delay, std_sender_delay, num_nodes, length(time));
-receiver_delay = normrnd(mean_receiver_delay, std_receiver_delay, num_nodes, length(time));
-crystal_freq = normrnd(crystal_freq_mean, crystal_freq_std, num_nodes, 1);
+t = (0:sampling_rate:duration-sampling_rate);
+
+clock_drift = zeros(num_nodes, length(t));
+offset_change = zeros(num_nodes, length(t));
 
 for node = 1:num_nodes
-    drift_ppm = ((crystal_freq(node) - crystal_freq_mean) / crystal_freq_mean) * 1e6; % ppm del drift
-    drift_std = drift_ppm / 1e6 * crystal_freq_mean; % Desviación estandar del drift
-    drift = normrnd(0, drift_std, 1, length(time)); 
-    clock_drift(node, :) = drift;
+    clock_drift(node, :) = sqrt(duration) * drift_rate * randn(size(t));    
+    offset_change(node, :) = linspace(0, drift_rate * duration, length(t));
 end
 
-time_deviation = zeros(num_nodes, length(time));
-for node = 1:num_nodes
-    for t = 2:length(time)
-        time_deviation(node, t) = time_deviation(node, t-1) + clock_drift(node, t) * time_step;
-    end
-end
+clock_error = clock_drift + offset_change;
+clock_error_seconds = clock_error / crystal_freq_mean;
+
+figure;
+plot(t, clock_error_seconds), xlabel('Timepo (segundos)'), ylabel('Error del reloj (segundos)')
+grid on
+title('Clock Drift')
+legend('Nodo 1', 'Nodo 2', 'Nodo 3', 'Nodo 4', 'Nodo 5', 'Nodo 6', 'Nodo 7', 'Nodo 8', 'Nodo 9', 'Nodo 10')
