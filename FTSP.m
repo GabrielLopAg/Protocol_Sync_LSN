12a6ce9447193749cea23de223bab5c0c59800a1
Angel Mares <chuisangel8@gmail.com>
2024-04-21 13:42:31 -0600

    Correccion de escala de tiempo

--- FTSP.m
+++ FTSP.m
@@ -1,25 +1,29 @@
+clear variables
 % Parámetros de la simulación
 num_nodos = 10; % Número de nodos en la red
 tiempo_simulacion = 1000; % Tiempo total de simulación en segundos
-intervalo_muestreo = 1; % Intervalo de muestreo en segundos
+dt                = 1;    % Resolución de la simulación en segundos
+intervalo_muestreo = 0; % Intervalo de muestreo en segundos
 frecuencia_oscilador = 32768; % Frecuencia del oscilador en Hz
 desviacion_oscilador = 20; % Desviación del oscilador en ppm
-frecuencia_muestreo = 1; % Frecuencia de muestreo en Hz
+frecuencia_muestreo = 0; % Frecuencia de muestreo en Hz
 
 % Inicialización de variables
 relojes_nodos = zeros(num_nodos, 1); % Inicialización de los relojes de los nodos
 offset_inicial = 0; % Offset inicial de tiempo
 
 % Bucle de simulación
-for t = 1:tiempo_simulacion
+for t = 1:dt:tiempo_simulacion
     % Generar offset aleatorio
-    offset_aleatorio = rand() * 2 * desviacion_oscilador - desviacion_oscilador;
+    offset_aleatorio = dt * desviacion_oscilador / 1e6 * ( rand(num_nodos, 1)*2 - 1 );
     
     % Actualizar relojes de los nodos con el offset aleatorio
-    relojes_nodos = relojes_nodos + offset_aleatorio;
+    relojes_nodos = relojes_nodos + dt + offset_aleatorio;
     
-    % Sincronizar relojes cada cierto intervalo de muestreo
+    % Sincronizar relojes cada cierto intervalo de muestreo 
+    % con el algoritmo de FTSP
     if mod(t, frecuencia_muestreo) == 0
+        % CORREGIR todo dentro del if, esto no es FTSP
         % Calcular el promedio de los relojes de los nodos
         tiempo_promedio = mean(relojes_nodos);
         
@@ -32,10 +36,8 @@ for t = 1:tiempo_simulacion
     
     % Simular la transmisión de los paquetes de sincronización cada segundo
     if mod(t, intervalo_muestreo) == 0
-        % Cálculo del tiempo actual
-        tiempo_actual = t / frecuencia_oscilador;
         
         % Envío de paquete de sincronización con el tiempo actual
-        disp(['Nodo 1 envía paquete de sincronización en el tiempo ', num2str(tiempo_actual)]);
+        disp(['Nodo 1 envía paquete de sincronización en el tiempo ', num2str(t)]);
     end
 end
